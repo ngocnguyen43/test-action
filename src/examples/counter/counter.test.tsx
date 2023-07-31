5917b35c0418ffa8a3c7dd13139ee4dd967fa431
Nguyen Minh Ngoc <minhngocnguyen.403@gmail.com>
2023-07-31 19:00:07 +0700

    :rabbit:

--- src/examples/counter/counter.test.tsx
+++ src/examples/counter/counter.test.tsx
@@ -1,10 +1,21 @@
-import { screen, render } from '@testing-library/react';
+// @vitest-environment happy-dom 
+
+import { screen, render, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
 
-test.todo('it should render the component', () => {});
+test('it should render the component', () => {
+  render(<Counter />)
+});
 
-test.todo(
+test(
   'it should increment when the "Increment" button is pressed',
-  async () => {},
+  async () => {
+    render(<Counter />)
+    const currentCount = screen.getByTestId("current-count")
+    expect(currentCount).toHaveTextContent("0")
+    const button = screen.getByRole("button", { name: "Increment" })
+    fireEvent.click(button)
+    expect(currentCount).toHaveTextContent("1")
+  },
 );
--- src/examples/counter/test/utilities.ts
+++ src/examples/counter/test/utilities.ts
@@ -1,5 +1,4 @@
-export {};
-
+import "@testing-library/jest-dom/extend-expect"
 /**
  * For a complete example, see: test/utilities.ts
  */
