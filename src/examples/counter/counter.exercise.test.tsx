669432161c9fb4362d768fb9d27c42f3b10cc5f0
Nguyen Minh Ngoc <minhngocnguyen.403@gmail.com>
2023-08-01 21:26:52 +0700

    coverage threshold

--- src/examples/counter/counter.exercise.test.tsx
+++ src/examples/counter/counter.exercise.test.tsx
@@ -1,7 +1,9 @@
-import { screen, render } from '@testing-library/react';
+// @vitest-environment happy-dom 
+
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Counter from '.';
-
+import { render } from "./test/utilities"
 test('it should render the component', () => {
   render(<Counter />);
   const currentCount = screen.getByTestId('current-count');
@@ -20,9 +22,21 @@ test('it should increment when the "Increment" button is pressed', async () => {
   expect(currentCount).toHaveTextContent('1');
 });
 
-test.todo('it should render the component with an initial count', () => {});
+test('it should render the component with an initial count', () => {
+  render(<Counter initialCount={1000} />)
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount).toHaveTextContent("1000")
 
-test.todo(
+});
+
+test(
   'it should reset the count when the "Reset" button is pressed',
-  async () => {},
+  async () => {
+    const { user } = render(<Counter initialCount={999} />)
+    const currentCount = screen.getByTestId('current-count');
+    const resetButton = screen.getByRole('button', { name: 'Reset' });
+    await user.click(resetButton)
+    expect(currentCount).toHaveTextContent("0")
+
+  },
 );
--- src/examples/counter/test/utilities.ts
+++ src/examples/counter/test/utilities.ts
@@ -1,4 +1,18 @@
-import "@testing-library/jest-dom/extend-expect"
+import '@testing-library/jest-dom/extend-expect';
 /**
  * For a complete example, see: test/utilities.ts
  */
+import type { ReactElement } from 'react';
+import { render as renderComponent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+type RenderOptions = Parameters<typeof renderComponent>[1];
+
+export * from '@testing-library/react';
+
+export const render = (ui: ReactElement, options?: RenderOptions) => {
+  return {
+    ...renderComponent(ui, options),
+    user: userEvent.setup(),
+  };
+};
